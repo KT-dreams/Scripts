890ec3fe3558102d7b186e4ca1e2ac8f89030339
Tomasz Abramczyk <Tomasz.Abramczyk@ringieraxelspringer.pl>
2020-08-29 20:05:21 +0200

    Start and stop scripts added.

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+VERSION=$1
+
+if [ -z "$VERSION" ] 
+then
+    echo 'Usage: ./install.sh <VERSION>'
+    exit 1
+fi
+
 git clone git@github.com:KT-dreams/IaC.git
 git clone git@github.com:KT-dreams/Backend.git
 
 mkdir data
 sudo chmod 664 data
-sudo docker run -p 8000:8000 -v $(pwd)/data:/data/ -d \
+sudo docker run -p 8000:8000 -v `pwd`/data:/data/ -d \
   amazon/dynamodb-local -jar DynamoDBLocal.jar \
   -sharedDb -dbPath /data
 
@@ -24,4 +32,4 @@ aws ecr get-login-password --region eu-central-1 | \
   358547536439.dkr.ecr.eu-central-1.amazonaws.com 
 
 sudo docker run -p 7000:8000 -v $BASE_PATH/Backend:/app -it \
-358547536439.dkr.ecr.eu-central-1.amazonaws.com/hacker-game:dev-0.1
+358547536439.dkr.ecr.eu-central-1.amazonaws.com/hacker-game:$VERSION
